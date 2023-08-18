4443ec6a1ebc2810f90201e01df754fdc5e96ebe
Daniel Esteban <decepeda@misena.edu.co>
2023-08-18 07:15:01 -0500

    Se agrega otro observador (log) y modificamos el stream reactivo

--- src/main/java/com/danielcepeda/webflux/app/controller/ProductController.java
+++ src/main/java/com/danielcepeda/webflux/app/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.danielcepeda.webflux.app.controller;
 
 import com.danielcepeda.webflux.app.models.dao.ProductDao;
 import com.danielcepeda.webflux.app.models.documents.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +13,20 @@ import reactor.core.publisher.Flux;
 public class ProductController {
 
     private final ProductDao dao;
-
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     public ProductController(ProductDao dao) {
         this.dao = dao;
     }
+
     @GetMapping({"/listar", "/"})
     public String getProducts(Model model) {
 
-        Flux<Product> products = dao.findAll();
+        Flux<Product> products = dao.findAll().map(product -> {
+                product.setName(product.getName().toUpperCase());
+                return product;
+        });
+        products.subscribe(product -> log.info(product.getName()));
+
         model.addAttribute("productos", products);
         model.addAttribute("titulo", "Listado de productos");
         return "listar";
